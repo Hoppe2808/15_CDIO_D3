427179329b64c2d11f7e9d30cd1e6be61e723fb6
SebbyG <seb.hop@hotmail.com>
2016-04-18 11:48:00 +0200

    Arbejde med webting

--- 15_CDIO_D3/src/tempName/client/_5_CDIO_D3.java
+++ 15_CDIO_D3/src/tempName/client/_5_CDIO_D3.java
@@ -8,6 +8,7 @@ import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.KeyCodes;
 import com.google.gwt.event.dom.client.KeyUpEvent;
 import com.google.gwt.event.dom.client.KeyUpHandler;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.DialogBox;
@@ -38,7 +39,7 @@ public class _5_CDIO_D3 implements EntryPoint {
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
-		final Button loginButton = new Button("Send");
+		final Button submitButton = new Button("Send");
 		final TextBox username = new TextBox();
 		final TextBox cprField = new TextBox();
 		final TextBox passwordField = new TextBox();
@@ -50,7 +51,7 @@ public class _5_CDIO_D3 implements EntryPoint {
 		final Label errorLabel = new Label();
 
 		// We can add style names to widgets
-		loginButton.addStyleName("sendButton");
+		submitButton.addStyleName("sendButton");
 		
 		adminYes.setValue(false);
 		adminNo.setValue(true);
@@ -65,7 +66,7 @@ public class _5_CDIO_D3 implements EntryPoint {
 		RootPanel.get("nameFieldContainer").add(username);
 		RootPanel.get("cprFieldContainer").add(cprField);
 		RootPanel.get("passwordFieldContainer").add(passwordField);
-		RootPanel.get("sendButtonContainer").add(loginButton);
+		RootPanel.get("sendButtonContainer").add(submitButton);
 		RootPanel.get("isAdminButtonContainer").add(panel);
 		RootPanel.get("errorLabelContainer").add(errorLabel);
 
@@ -73,40 +74,13 @@ public class _5_CDIO_D3 implements EntryPoint {
 		username.setFocus(true);
 		username.selectAll();
 
-		// Create the popup dialog box
-		final DialogBox dialogBox = new DialogBox();
-		dialogBox.setText("Remote Procedure Call");
-		dialogBox.setAnimationEnabled(true);
-		final Button closeButton = new Button("Close");
-		// We can set the id of a widget by accessing its Element
-		closeButton.getElement().setId("closeButton");
-		final Label textToServerLabel = new Label();
-		final HTML serverResponseLabel = new HTML();
-		VerticalPanel dialogVPanel = new VerticalPanel();
-		dialogVPanel.addStyleName("dialogVPanel");
-		dialogVPanel.add(new HTML("<b>Sending name to the server:</b>"));
-		dialogVPanel.add(textToServerLabel);
-		dialogVPanel.add(new HTML("<br><b>Server replies:</b>"));
-		dialogVPanel.add(serverResponseLabel);
-		dialogVPanel.setHorizontalAlignment(VerticalPanel.ALIGN_RIGHT);
-		dialogVPanel.add(closeButton);
-		dialogBox.setWidget(dialogVPanel);
-
-		// Add a handler to close the DialogBox
-		closeButton.addClickHandler(new ClickHandler() {
-			public void onClick(ClickEvent event) {
-				dialogBox.hide();
-				loginButton.setEnabled(true);
-				loginButton.setFocus(true);
-			}
-		});
-
 		// Create a handler for the sendButton and nameField
 		class MyHandler implements ClickHandler, KeyUpHandler {
 			/**
 			 * Fired when the user clicks on the sendButton.
 			 */
 			public void onClick(ClickEvent event) {
+				Window.alert(username.getText());
 				sendNameToServer();
 			}
 
@@ -126,39 +100,20 @@ public class _5_CDIO_D3 implements EntryPoint {
 				// First, we validate the input.
 				errorLabel.setText("");
 				String textToServer = username.getText();
+				Window.alert(username.getText());
 				if (!FieldVerifier.isValidName(textToServer)) {
 					errorLabel.setText("Please enter at least four characters");
 					return;
 				}
 
 				// Then, we send the input to the server.
-				loginButton.setEnabled(false);
-				textToServerLabel.setText(textToServer);
-				serverResponseLabel.setText("");
-				greetingService.greetServer(textToServer, new AsyncCallback<String>() {
-					public void onFailure(Throwable caught) {
-						// Show the RPC error message to the user
-						dialogBox.setText("Remote Procedure Call - Failure");
-						serverResponseLabel.addStyleName("serverResponseLabelError");
-						serverResponseLabel.setHTML(SERVER_ERROR);
-						dialogBox.center();
-						closeButton.setFocus(true);
-					}
-
-					public void onSuccess(String result) {
-						dialogBox.setText("Remote Procedure Call");
-						serverResponseLabel.removeStyleName("serverResponseLabelError");
-						serverResponseLabel.setHTML(result);
-						dialogBox.center();
-						closeButton.setFocus(true);
-					}
-				});
+				submitButton.setEnabled(false);
 			}
 		}
 
 		// Add a handler to send the name to the server
 		MyHandler handler = new MyHandler();
-		loginButton.addClickHandler(handler);
+		submitButton.addClickHandler(handler);
 		username.addKeyUpHandler(handler);
 	}
 }
