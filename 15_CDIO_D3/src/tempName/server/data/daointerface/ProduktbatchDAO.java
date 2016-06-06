a9cd519982c581f5969bb4b3a3c89dd4de22094c
mariamsaidi <mariamsaidi>
2016-06-06 15:38:10 +0200

    RaavareBatchDAO

--- 15_CDIO_D3/src/tempName/server/data/daointerface/ProduktbatchDAO.java
+++ 15_CDIO_D3/src/tempName/server/data/daointerface/ProduktbatchDAO.java
@@ -6,8 +6,8 @@ import tempName.server.data.dto.ProduktbatchDTO;
 
 public interface ProduktbatchDAO {
 
-	ProduktbatchDAO getProduktbatch(int pbId) throws DALException;
+	ProduktbatchDTO getProduktbatch(int pbId) throws DALException;
 	List<ProduktbatchDTO> getProduktbatchList( int pbId) throws DALException;
-	void createRaavareBatch(ProduktbatchDTO pbId) throws DALException;
-	void updateRaavareBatch(ProduktbatchDTO pbId) throws DALException;
+	void createRaavareBatch(ProduktbatchDTO pb) throws DALException;
+	void updateRaavareBatch(ProduktbatchDTO pb) throws DALException;
 }
