562939a4208d0dfe0d3c4998dff198121ac1289e
mariamsaidi <mariamsaidi>
2016-06-07 12:43:43 +0200

    recept og ProduktbatchKomponentDTO

---
+++ 15_CDIO_D3/src/tempName/server/data/dto/ProduktbatchKomponentDTO.java
@@ -0,0 +1,68 @@
+package tempName.server.data.dto;
+
+public class ProduktbatchKomponentDTO {
+
+		int pbId, rbId, oprId;
+		double tara, netto;
+
+
+		public ProduktbatchKomponentDTO(int pbId, int rbId, int oprId, double tara, double netto){
+			this.pbId = pbId;
+			this.rbId = rbId;
+			this.oprId=oprId;
+			this.tara= tara;
+			this.netto=netto;		
+		}
+
+		public int getPbId() {
+			return pbId;
+		}
+
+
+		public void setPbId(int pbId) {
+			this.pbId = pbId;
+		}
+
+
+		public int getRbId() {
+			return rbId;
+		}
+
+
+		public void setRbId(int rbId) {
+			this.rbId = rbId;
+		}
+
+
+		public int getOprId() {
+			return oprId;
+		}
+
+
+		public void setOprId(int oprId) {
+			this.oprId = oprId;
+		}
+
+
+		public double getTara() {
+			return tara;
+		}
+
+
+		public void setTara(double tara) {
+			this.tara = tara;
+		}
+
+
+		public double getNetto() {
+			return netto;
+		}
+
+
+		public void setNetto(double netto) {
+			this.netto = netto;
+		}
+		public String toString(){
+			return pbId + " " +  rbId  + " " +tara+" "+ netto+ " " + oprId;
+		}
+	}
---
+++ 15_CDIO_D3/src/tempName/server/data/dto/ReceptKomponentDTO.java
@@ -0,0 +1,42 @@
+package tempName.server.data.dto;
+
+public class ReceptKomponentDTO {
+
+	int recept_Id, raavare_Id;
+	double nom_netto, tolerance;
+
+	public ReceptKomponentDTO (int recept_Id, int raavare_Id,double nom_netto, double tolerance){
+		this.recept_Id = recept_Id;
+		this.raavare_Id = raavare_Id;
+		this.nom_netto=nom_netto;
+		this.tolerance=tolerance;
+	}
+	public int getRecept_Id() {
+		return recept_Id;
+	}
+	public void setRecept_Id(int recept_Id) {
+		this.recept_Id = recept_Id;
+	}
+	public int getRaavare_Id() {
+		return raavare_Id;
+	}
+	public void setRaavare_Id(int raavare_Id) {
+		this.raavare_Id = raavare_Id;
+	}
+	public double getNom_netto() {
+		return nom_netto;
+	}
+	public void setNom_netto(double nom_netto) {
+		this.nom_netto = nom_netto;
+	}
+	public double getTolerance() {
+		return tolerance;
+	}
+	public void setTolerance(double tolerance) {
+		this.tolerance = tolerance;
+	}
+	public String toString(){
+		return recept_Id + " " +  raavare_Id  + " " +nom_netto+" "+ tolerance;
+	}
+
+}
