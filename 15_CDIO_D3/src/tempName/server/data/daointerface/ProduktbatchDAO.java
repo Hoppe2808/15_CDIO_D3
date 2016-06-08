f7df9b015f3d4011e127a2875397c0afbd46ac9e
Magnus Haakonsson <magnusmisbat@gmail.com>
2016-06-08 13:41:50 +0200

    Tilføjet produktbatchkomp metoder

--- 15_CDIO_D3/src/tempName/server/data/daointerface/ProduktbatchDAO.java
+++ 15_CDIO_D3/src/tempName/server/data/daointerface/ProduktbatchDAO.java
@@ -3,20 +3,21 @@ package tempName.server.data.daointerface;
 import java.util.List;
 
 import tempName.server.data.daointerface.DALException;
-import tempName.shared.dto.ProduktBatchKomponentDTO;
 import tempName.shared.dto.ProduktBatchDTO;
+import tempName.shared.dto.ProduktBatchKompDTO;
 
 public interface ProduktbatchDAO {
 
+	// Methods for acces to the produktbatch table 
 	ProduktBatchDTO getProduktBatch(int pbId) throws DALException;
 	List<ProduktBatchDTO> getProduktBatchList() throws DALException;
 	void createProduktBatch(ProduktBatchDTO pb) throws DALException;
 	void updateProduktBatch(ProduktBatchDTO pb) throws DALException;
 
-	//Methods for acces to the produktbatchkomponent table
-	ProduktBatchKomponentDTO getProduktBatchKomp(int pbId, int rbId) throws DALException;
-	List<ProduktBatchKomponentDTO> getProduktBatchKompList(int pbId) throws DALException;
-	List<ProduktBatchKomponentDTO> getProduktBatchKompList() throws DALException;
-	void createProduktBatchKomp(ProduktBatchKomponentDTO produktbatchkomponent) throws DALException;
-	void updateProduktBatchKomp(ProduktBatchKomponentDTO produktbatchkomponent) throws DALException;	
+	// Methods for acces to the produktbatchkomponent table
+	ProduktBatchKompDTO getProduktBatchKomp(int pbId, int rbId) throws DALException;
+	List<ProduktBatchKompDTO> getProduktBatchKompList(int pbId) throws DALException;
+	List<ProduktBatchKompDTO> getProduktBatchKompList() throws DALException;
+	void createProduktBatchKomp(ProduktBatchKompDTO produktbatchkomp) throws DALException;
+	void updateProduktBatchKomp(ProduktBatchKompDTO produktbatchkomp) throws DALException;	
 }
