72a2f101fe000678e451083e0403fd8554ddf743
SebbyG <seb.hop@hotmail.com>
2016-06-10 11:13:37 +0200

    Rettet fejl i receptMenu

--- 15_CDIO_D3/src/tempName/client/gui/ReceptMenu.java
+++ 15_CDIO_D3/src/tempName/client/gui/ReceptMenu.java
@@ -7,6 +7,7 @@ import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.cellview.client.CellTable;
 import com.google.gwt.user.cellview.client.TextColumn;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.ListBox;
@@ -19,7 +20,7 @@ import tempName.shared.dto.RaavareDTO;
 import tempName.shared.dto.ReceptDTO;
 
 public class ReceptMenu {
-	
+
 	private VerticalPanel container;
 	private int prevMenu;
 	private ArrayList<ReceptDTO> recept;
@@ -32,7 +33,7 @@ public class ReceptMenu {
 		this.recept = recept;
 		this.mainGUI = mainGUI;
 
-}
+	}
 	public void recept(){
 		headerLabel.setText("Recept");;
 		headerLabel.addStyleName("HeaderLabel");
@@ -46,13 +47,13 @@ public class ReceptMenu {
 		container.add(addButton);
 		container.add(updateButton);
 		container.add(back);
-		
+
 		inspectButton.addClickHandler(new ClickHandler() {
 			public void onClick(ClickEvent event) {
 				inspectRecept();
 			}
 		});
-		
+
 		addButton.addClickHandler(new ClickHandler() {
 			public void onClick(ClickEvent event) {
 				addRecept();
@@ -63,14 +64,14 @@ public class ReceptMenu {
 				updateRecept();
 			}
 		});
-		
+
 		back.addClickHandler(new ClickHandler() {
 			public void onClick(ClickEvent event) {
 				mainGUI.adminCheck(prevMenu);
 			}
 		});
 	}
-	
+
 	public void inspectRecept() {
 		CellTable<ReceptDTO> table = new CellTable<ReceptDTO>();
 		TextColumn<ReceptDTO> receptId = new TextColumn<ReceptDTO>(){
@@ -89,10 +90,10 @@ public class ReceptMenu {
 			}
 
 		};
-	
+
 
 		table.addColumn(receptId, "Recept Id");
-		table.addColumn(receptName, "Recept Name");
+		table.addColumn(receptName, "Recept Navn");
 
 
 		ListDataProvider<ReceptDTO> dataProvider = new ListDataProvider<ReceptDTO>();
@@ -125,24 +126,24 @@ public class ReceptMenu {
 		final TextBox navn = new TextBox();
 		final Button submit = new Button("Opret");
 		final Button back = new Button("<- Tilbage");
-		final Label message = new Label();
 		container.clear();
 		container.add(headerLabel);
 		container.add(navnLbl);
 		container.add(navn);
 		container.add(submit);
 		container.add(back);
-		container.add(message);
-		
-		
+
 		submit.addClickHandler(new ClickHandler() {
 			public void onClick(ClickEvent event) {
+				if (navn.getText().isEmpty()){
+					Window.alert("En recept skal have et navn");
+				} else {
 				mainGUI.serviceImpl.addRecept(navn.getText());
 				mainGUI.adminCheck(prevMenu);
-				message.setText("Recept added");
+				}
 			}
 		});
-		
+
 		back.addClickHandler(new ClickHandler() {
 			public void onClick(ClickEvent event) {
 				recept();
@@ -150,14 +151,14 @@ public class ReceptMenu {
 		});
 	}
 	public void updateRecept(){
-		headerLabel.setText("Update Recept");
-		final Label idLabel = new Label("Enter ID for Recept:");
+		headerLabel.setText("Opdater Recept");
+		final Label idLabel = new Label("Vælg navnet for den recept du vil ændre:");
 		final ListBox lb = new ListBox();
-		final Label navnLabel = new Label("Enter Recept Name:");
+		final Label navnLabel = new Label("Indtast nyt navn for recept:");
 		final TextBox navn = new TextBox();
-		final Button submit = new Button("Update");
-		final Button delete = new Button("Delete");
-		final Button back = new Button("<- Back");
+		final Button submit = new Button("Opdater");
+		final Button delete = new Button("Slet");
+		final Button back = new Button("<- Tilbage");
 		for (int i = 0; i < recept.size(); i++){
 			lb.addItem(recept.get(i).getReceptName());
 		}
@@ -171,23 +172,27 @@ public class ReceptMenu {
 		container.add(submit);
 		container.add(delete);
 		container.add(back);
-		
-		
-	
+
+
+
 		delete.addClickHandler(new ClickHandler() {
 			public void onClick(ClickEvent event) {
-				for (int i = 0; i < recept.size(); i++){
-					String rName = recept.get(i).getReceptName();
-					if (rName.equals(lb.getSelectedItemText())){
-						mainGUI.serviceImpl.deleteRecept(recept.get(i).getReceptId());
+					for (int i = 0; i < recept.size(); i++){
+						String rName = recept.get(i).getReceptName();
+						if (rName.equals(lb.getSelectedItemText())){
+							mainGUI.serviceImpl.deleteRecept(recept.get(i).getReceptId());
+						}
 					}
-				}
-				mainGUI.adminCheck(prevMenu);
+					mainGUI.adminCheck(prevMenu);
+
 			}
 		});
-		
+
 		submit.addClickHandler(new ClickHandler() {
 			public void onClick(ClickEvent event) {
+				if (navn.getText().isEmpty()){
+					Window.alert("En recept skal have et navn");
+				} else {
 				for (int i = 0; i < recept.size(); i++){
 					String rName = recept.get(i).getReceptName();
 					if (rName.equals(lb.getSelectedItemText())){
@@ -195,9 +200,10 @@ public class ReceptMenu {
 					}
 				}
 				mainGUI.adminCheck(prevMenu);
+				}
 			}
 		});
-		
+
 		back.addClickHandler(new ClickHandler() {
 			public void onClick(ClickEvent event) {
 				recept();
